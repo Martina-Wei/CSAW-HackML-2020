e6bc4f0f6a36b2dd7f7aa5345b0c72b65b5bfb95
Martina-Wei <xxxxxtsuki@gmail.com>
2020-12-21 21:25:29 -0800

    feat: PCA method

--- repair_generator.py
+++ repair_generator.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 import h5py
@@ -8,20 +9,15 @@ from sklearn.decomposition import PCA
 from sklearn.metrics import normalized_mutual_info_score
 
 from loader import load_data
-import argparse
 
 N_class = 1283
 parser = argparse.ArgumentParser()
 parser.add_argument("bd_model", help="bad model")
-# parser.add_argument("-n", help="treshold", type=float, dest="treshold")
 parser.add_argument("-v", "--validate", dest="validate_data_path")
 parser.add_argument("-t", "--test", dest="test_data_path")
-parser.add_argument("-p", "--poisoned", dest="poisoned_data_path")
 parser.add_argument("-m", "--mode", dest="mode", default="repair")
 
 args = parser.parse_args()
-
-
 
 class BD():
 
@@ -53,24 +49,28 @@ class BD():
             self.treshold[i] = score
             self.mean_vector[i] = mean_vec_x
     
-    def cast(self, vector_x, y, mode='fit'):
-        new_x = []
-        if mode == 'fit':
-            new_y = []
-            for i in range(N_class):
-                idx = (y==i)
-                self.pca[i] = PCA(n_components=3)
-                x = self.pca[i].fit_transform(vector_x[idx])
-                new_x.append(x)
-                new_y += [i]*sum(idx)
-            return np.array(new_x).reshape(-1, 3), np.array(new_y)
-        else:
-            for x_, y_ in zip(vector_x, y):
-                print(x_.shape)
-                x = self.pca.get(y_).transform(x_)
-                new_x.append(x)
-            return np.array(new_x)
+    def get_pca_treshold(self, vector_x, y):
+        for i in range(N_class):
+            idx = (y==i)
+            matrix = np.dot(self.mean_vector.get(i), np.transpose(vector_x[idx]))
+            self.treshold[i] = np.linalg.norm(matrix, axis=0, keepdims=True)
 
+
+    def fit_pca(self, vector_x, y=None):
+        pca =  PCA(n_components=2)
+        for i in range(N_class):
+            idx = (y==i)
+            x = pca.fit_transform(np.transpose(vector_x[idx]))
+            self.mean_vector[i] = np.transpose(x)
+    
+    def pca_filter(self, xs, ys):
+        result = [False] * len(ys)
+        for i, (x, y) in enumerate(zip(xs, ys)):
+            temp = np.dot(self.mean_vector.get(y), np.transpose(x))
+            if np.linalg.norm(temp) < np.min(self.treshold[y]) or  np.linalg.norm(temp) > np.max(self.treshold[y]):
+                result[i] = True
+        return result
+        
 
     def filter(self, xs, ys):
         result = [False] * len(ys)
@@ -83,10 +83,10 @@ class BD():
 
     def get_result(self, y_predict, idx):
         label = np.array(y_predict)
-        label[idx] = N_class+1
+        label[idx] = N_class
         return label
-
-
+    
+    
 def main():
     original_bd = keras.models.load_model(args.bd_model)
     bd = BD(original_bd)
@@ -95,24 +95,22 @@ def main():
     x_vali, y_vali = load_data(args.validate_data_path)
 
     # First Step -> get treshold
-
     vector_validation_x = bd.load_vector(x_vali)  
-    vector_validation_x, y_vali = bd.cast(vector_validation_x, y_vali, 'fit')
-    print(vector_validation_x.shape)
-    bd.get_treshold(vector_validation_x, y_vali)
+    bd.fit_pca(vector_validation_x, y_vali)
+    bd.get_pca_treshold(vector_validation_x, y_vali)
     
     # Second Step -> filter poisoned
-    x_test, y_test = load_data(args.test_data_path)
+    x_test, _ = load_data(args.test_data_path)
     y_test_predict = np.argmax(original_bd.predict(x_test), axis=1).reshape(-1)
     vector_test_x = bd.load_vector(x_test)
-    vector_test_x= bd.cast(vector_test_x, y_test_predict, 'other')
-    idx = bd.filter(vector_test_x, y_test_predict.tolist())
+    idx = bd.pca_filter(vector_test_x, y_test_predict)
     
-    # Third Step -> return correct label
-    label = bd.get_result(y_test_predict, idx)
-    print("{} / {} / {}".format(sum(idx), sum(label!=5), len(y_test)))
-    print(y_test_predict.tolist())
-    print(label.tolist())
+    print(sum(idx), len(y_test_predict))
+    # # Third Step -> return correct label
+    # label = bd.get_result(y_test_predict, idx)
+    # print("{} / {} / {}".format(sum(idx), sum(label!=5), len(y_test)))
+    # print(y_test_predict.tolist())
+    # print(label.tolist())
  
 
 
